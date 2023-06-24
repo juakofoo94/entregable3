eab095e918d80a161a14a932da3a8251f861ae85
Joaquin <jnavarocca@gmail.com>
2023-06-24 20:31:08 -0300

    Arme el template

--- entregable3/settings.py
+++ entregable3/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'entregable3.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- inicio/views.py
+++ inicio/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def inicio(request):
-    return HttpResponse ('<h1>Mi primera vista</h1>')
+    return render(request, 'inicio/inicio.html')
