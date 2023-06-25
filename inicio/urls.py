9b96d9e27a8499e74374ab968491d4eacf55a46b
Joaquin <jnavarocca@gmail.com>
2023-06-25 13:47:31 -0300

    Armando el form

--- inicio/urls.py
+++ inicio/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from inicio import views
 
+app_name = 'inicio'
+
 urlpatterns = [
-    path('', views.inicio, name='inicio')
+    path('', views.inicio, name='inicio'),
+    path('equipos/anotar/', views.anotar_equipo, name='anotar_equipo')
 ]
--- inicio/views.py
+++ inicio/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from inicio.forms import AnotarEquipoForm
+
+
+
 # Create your views here.
 
 def inicio(request):
     return render(request, 'inicio/inicio.html')
+
+def anotar_equipo(request):
+    
+    if request.method == 'POST':
+        formulario = AnotarEquipoForm(request.POST)
+        if formulario.is_valid():
+            info = formulario.cleaned_data
+    
+    
+    formulario = AnotarEquipoForm()
+    return render(request, 'inicio/anotar_equipo.html', {'formulario': formulario})
