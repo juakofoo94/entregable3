15c9c2f0273fcd85c8e6d6cf71b2dbf4630af114
Joaquin <jnavarocca@gmail.com>
2023-06-25 16:03:03 -0300

    Termine el entregable

--- inicio/views.py
+++ inicio/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from inicio.forms import AnotarEquipoForm
-
+from inicio.models import EquipoFutbol
 
 
 # Create your views here.
@@ -9,12 +9,18 @@ def inicio(request):
     return render(request, 'inicio/inicio.html')
 
 def anotar_equipo(request):
-    
+    mensaje = ''
     if request.method == 'POST':
         formulario = AnotarEquipoForm(request.POST)
         if formulario.is_valid():
             info = formulario.cleaned_data
-    
+            equipo = EquipoFutbol(nombre=info['nombre'], ciudad=info['ciudad'], edad=info['edad'])
+            equipo.save()    
+            mensaje = 'Tus datos se registraron correctamente'
+        
+        
+        else:
+            return render(request, 'inicio/anotar_equipo.html', {'formulario': formulario})
     
     formulario = AnotarEquipoForm()
-    return render(request, 'inicio/anotar_equipo.html', {'formulario': formulario})
+    return render(request, 'inicio/anotar_equipo.html', {'formulario': formulario, 'mensaje': mensaje})
